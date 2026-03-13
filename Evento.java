2d66551bf4e8f320bcf2f1a0d69eeffd1caffc84
gustavocaveca78-eng <gustavocaveca78@gmail.com>
2026-03-12 21:20:32 -0300

    Add Evento class with event details and display method

---
+++ Evento.java
@@ -0,0 +1,25 @@
+public class Evento {
+
+    String nome;
+    String endereco;
+    String categoria;
+    String horario;
+    String descricao;
+
+    public Evento(String nome, String endereco, String categoria, String horario, String descricao) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.categoria = categoria;
+        this.horario = horario;
+        this.descricao = descricao;
+    }
+
+    public void mostrarEvento() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Horário: " + horario);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("---------------------------");
+    }
+}
